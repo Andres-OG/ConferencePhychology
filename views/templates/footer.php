085e683bfe6f2a4bcee3e086c58c20732437c3d0
Andres OG <andresog@Andress-MacBook-Air.local>
2023-05-31 16:37:37 -0600

    Deployment app

---
+++ views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+            ConferencePsychology
+            </h3>
+
+            <p class="footer__texto">
+            ConferencePsychology es una conferencia para psicologos, se lleva a cabo de forma presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="menu-redes"> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">Tiktok</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="">
+                <span class="menu-redes__ocultar">Github</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+    ConferencePsychology
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
